ec3c6cf1b93e9834a91065d0226800383cd54f0e
Petraitis15 <leonardo.petraitis@etec.sp.gov.br>
2026-03-06 19:31:45 -0300

    Complemento agenda 2

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,12 +1,40 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+
+namespace MauiAppMinhasCompras
 {
+    //Agenda 03
     public partial class App : Application
     {
+        //Campo: onde o dado está gravado. Aqui: campo implicitamente privado. Underline indica campo.
+        static SQLiteDatabaseHelper _db;
+
+        //Propriedade: forma de acesso ao campo.
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    //Busca do caminho onde está o arquivo de banco de dados, pois cada plataforma
+                    //armazena em um caminho diferente.
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQLiteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
 
             //MainPage = new AppShell();
+            //Agenda 02
             MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -1,3 +1,6 @@
+using MauiAppMinhasCompras.Models;
+using System.Threading.Tasks;
+
 namespace MauiAppMinhasCompras.Views;
 
 public partial class NovoProduto : ContentPage
@@ -6,4 +9,28 @@ public partial class NovoProduto : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            //Variável p do tipo Produto recebe um novo objeto do tipo Produto.
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text)
+            };
+
+            //A partir do Db temos acesso aos métodos implementados na SQLiteDataBaseHelper
+            await App.Db.Insert(p);
+
+            await DisplayAlert("Sucesso!", "Registro Inserido", "OK");
+
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+        }
+    }
 }
